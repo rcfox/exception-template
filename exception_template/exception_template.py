89784e136d72e959fbcf56f7e89ec358b62ac815
Ryan Fox <ryan@rcfox.ca>
2019-08-06 08:30:43 -0400

    Add docstrings and doctests to the ExceptionTemplate class.

--- exception_template/exception_template.py
+++ exception_template/exception_template.py
@@ -1,10 +1,31 @@
 import string
 
 class ExceptionTemplate(Exception):
+    """
+    Base class for templated exceptions. Child classes need only set a format string as the message:
+
+    >>> class MyException(ExceptionTemplate):
+    ...     message = 'My exception occurred! Context: {context}'
+    >>> raise MyException(context='example')
+    Traceback (most recent call last):
+        ...
+    MyException: My exception occurred! Context: example
+
+    Exception arguments can also be retrieved by name as object members.
+    >>> e = MyException(context='example')
+    >>> e.context
+    'example'
+
+    A custom string formatter can also be supplied to override the default.
+    """
     message: str = ''
     formatter: string.Formatter = string.Formatter()
 
     def __init__(self, **kwargs: str) -> None:
+        """
+        Exception arguments are accepted as kwargs, but a check is performed to ensure that
+        all format string parameters are passed in, and no extras are given.
+        """
         super().__init__()
         self._kwargs = kwargs
         self._error_check()
@@ -27,7 +48,14 @@ class ExceptionTemplate(Exception):
         return self.formatter.format(self.message, **self._kwargs)
 
     def __getattr__(self, key: str) -> str:
+        """
+        Any exception kwargs arguments are accessible by name on the object.
+        """
         if key in self._kwargs:
             return self._kwargs[key]
         raise AttributeError("'{name}' object has no attribute '{key}'"
                              .format(name=self.__class__.__name__, key=key))
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
