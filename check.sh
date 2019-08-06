76427e183f1e10ca3c1cd98c627c5e608712adf0
Ryan Fox <ryan@rcfox.ca>
2019-08-06 09:05:46 -0400

    Update the check script to include the tests.

--- check.sh
+++ check.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-mypy exception_template
-pylint exception_template
+mypy exception_template tests
+pylint exception_template tests
+
+# Tests
+python3 exception_template/exception_template.py
+python3 -m unittest discover -s tests
